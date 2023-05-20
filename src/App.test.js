d58762b4c1e1f2c20d0661ec0f3aaabec478172b
maja669e <maja669e@stud.kea.dk>
2023-05-20 15:24:08 +0200

    app test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom'; // Import the appropriate router component
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  render(
+    <Router>
+      <App />
+    </Router>
+  );
+  // Rest of your test code...
 });
